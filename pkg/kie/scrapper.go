b5d72f34857294aac37f399fcec108e9df5c346b
Thibaut Gauvin <gauvin.thibaut83@gmail.com>
2024-03-16 21:30:55 +0100

    feat(metrics): add images list counter

--- pkg/kie/scrapper.go
+++ pkg/kie/scrapper.go
@@ -2,37 +2,36 @@ package kie
 
 import (
 	"context"
-	"time"
 
+	internalHandlers "github.com/Thibaut-gauvin/kie/internal/handlers"
 	"github.com/Thibaut-gauvin/kie/internal/logger"
+	"github.com/Thibaut-gauvin/kie/pkg/metrics"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
 // scrapClusterImages is the function called by cron scheduler that is responsible to refresh prometheus metrics.
 func scrapClusterImages(k8sClient kubernetes.Clientset) error {
-	logger.Infof("[%s] %s", clusterImageScrapJobName, time.Now().Format(time.RFC3339))
+	logger.Infof("tasks [%s] updated", clusterImageScrapJobName)
 
 	pods, err := k8sClient.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{})
 	if err != nil {
+		internalHandlers.UpdateHealthy(false)
 		return err
 	}
 
 	for _, pod := range pods.Items {
 		for _, container := range pod.Spec.Containers {
-			logger.Debugf(
-				"image: %s, pod: %s, namespace: %s",
-				container.Image, pod.Name, pod.Namespace,
-			)
+			metrics.ObserveClusterImagesCount(container.Image, container.Name, pod.Name, pod.Namespace)
+			logger.Debugf("image: %s, pod: %s, namespace: %s", container.Image, pod.Name, pod.Namespace)
 		}
 
 		for _, container := range pod.Spec.InitContainers {
-			logger.Debugf(
-				"image: %s, pod: %s, namespace: %s",
-				container.Image, pod.Name, pod.Namespace,
-			)
+			metrics.ObserveClusterImagesCount(container.Image, container.Name, pod.Name, pod.Namespace)
+			logger.Debugf("image: %s, pod: %s, namespace: %s", container.Image, pod.Name, pod.Namespace)
 		}
 	}
 
+	internalHandlers.UpdateHealthy(true)
 	return nil
 }
